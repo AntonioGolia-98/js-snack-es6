bce76b7e1dd6c7d2567f1bc5c6242da918e21762
AntonioGolia-98 <antoniog98@icloud.com>
2026-01-11 17:54:01 +0100

    creazione array oggeetto snack2

---
+++ snack-2/main.js
@@ -0,0 +1,8 @@
+const squadre = [
+    {nome : "Napoli", puntiFatti : 0, falliSubiti: 0},
+    {nome : "Milan", puntiFatti : 0, falliSubiti: 0},
+    {nome : "Juve", puntiFatti : 0, falliSubiti: 0},
+    {nome : "Venezia", puntiFatti : 0, falliSubiti: 0},
+    {nome : "Roma", puntiFatti : 0, falliSubiti: 0},
+    {nome : "Verona", puntiFatti : 0, falliSubiti: 0},
+]
